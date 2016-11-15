c562ea264f5ae664a88edba59a4b5aa5cc243fef
JingjieZhang <jingjie.zhang.146@my.csun.edu>
2016-11-15 09:47:01 -0800

    long click to get route between two point

---
+++ MapTest/app/src/main/java/com/example/jingjie/maptest/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.jingjie.maptest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent i=new Intent(MainActivity.this,mapActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
